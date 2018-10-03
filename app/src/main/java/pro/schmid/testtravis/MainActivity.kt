f54a8221db4d197b81abdeec617577e3726a8623
Jonas Schmid <jonas.schmid@gmail.com>
2018-10-03 12:17:23 +0200

    Initial commit

---
+++ app/src/main/java/pro/schmid/testtravis/MainActivity.kt
@@ -0,0 +1,12 @@
+package pro.schmid.testtravis
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
